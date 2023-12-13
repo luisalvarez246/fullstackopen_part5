7fce8b40d4f6242a025fea6142dbee6ef86c45b6
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-12-13 10:59:21 +0100

    add CreateBlog component

---
+++ src/components/CreateBlog.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import blogService from '../services/blogs'
+
+const CreateBlog = ({ setBlogs }) =>
+{
+	const	[formData, setFormData] = useState({
+		title: '',
+		author: '',
+		url: ''
+	})
+
+	const handleBlogCreation = async (event) =>
+	{
+		event.preventDefault();
+		try
+		{
+			await blogService.saveBlog(formData);
+			const	blogs = await blogService.getAll();
+			setFormData(
+			{
+				title: '',
+				author: '',
+				url: ''
+			})
+			setBlogs(blogs);
+		}
+		catch(error)
+		{
+			console.log(error);
+		}
+	};
+
+	return (
+		<>
+			<h2>Create new</h2>
+			<form onSubmit={handleBlogCreation}>
+				<fieldset>
+					<label htmlFor="Title">title</label>
+					<input 
+						name="Title"
+						id="Title"
+						value={formData.title}
+						onChange={({ target }) => setFormData(prevFormData => (
+						{
+							...prevFormData,
+							title: target.value
+						}))}
+						type='text'
+						required={true}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="Author">author</label>
+					<input 
+						name="Author"
+						id="Author"
+						value={formData.author}
+						onChange={({ target }) => setFormData(prevFormData => (
+						{
+							...prevFormData,
+							author: target.value
+						}))}
+						type='text'
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="Url">url</label>
+					<input 
+						name="Url"
+						id="Url"
+						value={formData.url}
+						onChange={({ target }) => setFormData(prevFormData => (
+						{
+							...prevFormData,
+							url: target.value
+						}))}
+						type='text'
+						required={true}
+					/>
+				</fieldset>
+				<button type="submit">create</button>
+			</form>
+		</>
+	);
+};
+
+export default CreateBlog;
