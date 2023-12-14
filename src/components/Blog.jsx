0edd597d51d1a35b893b385b798428947aacd74e
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-12-14 10:17:53 +0100

    add toggle functionality to blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import '../styles/Blog.css';
+
+const Blog = ({ blog }) => 
+{
+	const	[showFull, setShowFull] = useState(false);
+
+	const toggleBlog = () =>
+	{
+		setShowFull(!showFull);
+	};
+
+	return (
+		<>
+			{showFull ?
+				<section className="blog">
+					<div className="blog-title">
+						{blog.title} {blog.author}
+						<button onClick={toggleBlog} className="toggle-button">hide</button>
+					</div>
+					<p className="blog-details">{blog.url}</p>
+					<div className="blog-likes">
+						<p className="blog-details">likes {blog.likes}</p>
+						<button>like</button>
+					</div>
+					<p className="blog-details">{blog.user.username}</p>
+				</section> :
+				<section className="blog-title">
+					{blog.title} {blog.author}
+					<button onClick={toggleBlog} className="toggle-button">view</button>
+				</section>
+			}
+		</>  
+)}
 
 export default Blog
