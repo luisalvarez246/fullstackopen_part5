41c80bee79fb400269998e08be620c2ae2f6dce4
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-12-19 13:09:52 +0100

    test for frontpage

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,7 +1,46 @@
-describe('Blog App', () => {
+describe('Blog App', () =>
+{
+	const	user =
+	{
+		username: 'piyumby',
+		password: 'split1/2',
+		name: 'Bumby'
+	}
+
+	beforeEach(() =>
+	{
+		cy.request('POST', 'http://localhost:3003/api/testing/reset');
+		// cy.request('POST', 'http://localhost:3003/api/users', user);
+		cy.visit('http://localhost:5173');
+	})
+
 	it('front page can be opened', () =>
 	{
-		cy.visit('http://localhost:5173');
 		cy.contains('log in to application');
 	})
+
+	it('frontpage shows login form', () =>
+	{
+		cy.get('form').within(() =>
+		{
+			cy.get('input[name="username"]')
+				.should('exist')
+				.should('have.attr', 'type', 'text');
+			cy.get('input[name="password"]')
+				.should('exist')
+				.should('have.attr', 'type', 'password');
+			cy.get('#login-button')
+				.should('exist')
+				.should('have.attr', 'type', 'submit');
+		})
+	})
+
+	// it('login form can be opened', () =>
+	// {
+	// 	cy.get('#username').type(user.username);
+	// 	cy.get('#password').type(user.password);
+	// 	cy.get('#login-button').click();
+
+	// 	cy.contains(`${user.username} logged in`);
+	// })
 })
