361ffcd08b5cbfb530b92956eed7f455e00d8f3f
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-12-20 09:14:17 +0100

    add custom commands login and createBlog

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// -- This will bypass login UI --
+Cypress.Commands.add('login', (user) =>
+{
+	cy.request('POST', `${Cypress.env('BACKEND')}/login`, user)
+		.then(({ body }) =>
+		{
+			localStorage.setItem('loggedUser', JSON.stringify(body));
+			cy.visit('');
+		})
+})
+
+// -- This will add a new blog --
+Cypress.Commands.add('createBlog', ({ title, author, url }) =>
+{
+	cy.request(
+		{
+			url: `${Cypress.env('BACKEND')}/blogs`,
+			method: 'POST',
+			body: { title, author, url },
+			headers:
+			{
+				Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedUser')).token}`
+			}
+		})
+	cy.visit('');
+})
